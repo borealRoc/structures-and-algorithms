60a000c6364b74de7d01881275b267654659210e
xusp <xspstudyemail@163.com>
2022-11-23 15:44:29 +0800

    feat: add linkedlist mutil pointer method

--- src/algorithms/linked-list-application/mutil-pointer-method.js
+++ src/algorithms/linked-list-application/mutil-pointer-method.js
@@ -22,15 +22,48 @@ export function deleteNEnd(l, n) {
 }
 
 // 2. 多指针法——链表的反转
-// export function reverseList(l) {
-//   let pre = null;
-//   let cur = l;
-//   pre.next = cur;
+export function reverseList(l) {
+  let pre = null;
+  let cur = l;
 
-//   while (cur) {
-//     let later = cur.next
+  while (cur) {
+    let next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+  }
 
-//     later.next
-//     cur = cur.next;
-//   }
-// }
+  return pre;
+}
+
+// 3. 反转第 m 到 第n （1 ≤ m ≤ n ≤ 链表长度）的链表（使用一趟扫描完成反转）
+export function reverseFromM2N(l, m, n) {
+  let dummy = new ListNode();
+  dummy.next = l;
+
+  // 1. 用left缓存m之前的元素
+  let pre = dummy;
+  let left = null;
+  for (let i = 1; i < m; i++) {
+    pre = pre.next;
+  }
+  left = pre;
+
+  // 2. 翻转m到n之间的元素
+  let start = pre.next;
+  pre = start;
+  let cur = start.next;
+
+  for (let i = m; i < n; i++) {
+    let next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+  }
+
+  // 3. 翻转完成后，让left->翻转后的链表->cur
+  left.next = pre;
+  start.next = cur;
+
+  return l;
+}
--- src/algorithms/linked-list-application/test.js
+++ src/algorithms/linked-list-application/test.js
@@ -1,62 +1,104 @@
 import ListNode, { List } from "./ListNode";
+
 // 1. 链表基础操作：手动操作
-// 1. 初始化：传入 val, 指定 next（不指定则为null）
-const l = new ListNode(1);
-l.next = new ListNode(3);
-console.log("链表初始化", l);
-// 2. 链表元素的追加：在链表尾部指定next
-l.next.next = new ListNode(4);
-console.log("链表追加", l);
-// 3. 在链表两节点之间添加节点：前一个节点.next = 插入，插入.next = 后一个节点
-const ln = new ListNode(2);
-const pre = l;
-const later = l.next;
-pre.next = ln;
-ln.next = later;
-console.log("链表插入", l);
-// 4. 链表元素的删除：前一个节点.next = 前一个节点.next.next
-const pre1 = l.next;
-pre1.next = pre1.next.next;
-console.log("链表元素的删除", l);
-// 5. 链表元素的访问：从第一个节点遍历，直到index
-const index = 2;
-let current = l;
-for (let i = 0; i < index; i++) {
-  current = current.next;
+function listNodeDemo() {
+  // 1. 初始化：传入 val, 指定 next（不指定则为null）
+  const ln = new ListNode(1);
+  ln.next = new ListNode(3);
+  console.log("链表初始化", ln); // 1->3->null
+
+  // 2. 链表元素的追加：在链表尾部指定next
+  const ln1 = JSON.parse(JSON.stringify(ln));
+  ln1.next.next = new ListNode(4);
+  console.log("链表追加", ln1); // 1->3->4->null
+
+  // 3. 在链表两节点之间添加节点：前一个节点.next = 插入，插入.next = 后一个节点
+  const ln2 = JSON.parse(JSON.stringify(ln1));
+  const node = new ListNode(2);
+  const later = ln2.next;
+  ln2.next = node;
+  node.next = later;
+  console.log("链表插入", ln2); // 1->2->3->4->null
+
+  // 4. 链表元素的删除：前一个节点.next = 前一个节点.next.next
+  const ln3 = JSON.parse(JSON.stringify(ln2));
+  const pre1 = ln3.next;
+  pre1.next = pre1.next.next;
+  console.log("链表元素的删除", ln3); // 1->3->4->null
+
+  // 5. 链表元素的访问：从第一个节点遍历，直到index
+  const index = 2;
+  let current = ln3;
+  for (let i = 0; i < index; i++) {
+    current = current.next;
+  }
+  console.log("链表第三个节点是", current); // 4->null
 }
-console.log("链表第三个节点是", current);
+listNodeDemo();
 
 // 2. 链表基础操作：通过 List 类提供的方法操作
-const list = new List(1)
-console.log('链表初始化', list)
-list.push(3)
-list.push(4)
-console.log("链表追加", list);
-list.insert(2, 1)
-console.log("链表插入", list);
-list.remove(2)
-console.log("链表元素的删除", list);
-const node2 = list.get(2)
-console.log("链表第三个节点是", node2);
+function listDemo() {
+  const l = new List(1);
+  console.log("链表初始化", l); // 1->null
 
-// 3. 链表单指针法的应用 
-import { concatSortedLinkedlist, pickRepeatLinkedlist,deleteRepeatLinkedlist } from "./single-pointer-method";
-const twoLists = concatSortedLinkedlist(l, list.head);
+  l.push(3);
+  l.push(4);
+  console.log("链表追加", l); // 1->3->4->null
+
+  l.insert(2, 1);
+  console.log("链表插入", l); // 1->2->3->4->null
+
+  l.remove(2);
+  console.log("链表元素的删除", l); // 1->2->4->null
+
+  const current2 = l.get(2);
+  console.log("链表第三个节点是", current2); // 4->null
+}
+listDemo();
+
+const list = new ListNode(1);
+const listNode2 = (list.next = new ListNode(1));
+const listNode3 = (listNode2.next = new ListNode(2));
+const listNode4 = (listNode3.next = new ListNode(3));
+const listNode5 = (listNode4.next = new ListNode(4));
+const listNode6 = (listNode5.next = new ListNode(4));
+listNode6.next = new ListNode(4);
+console.log("list", list); // 1->1->2->3->4->4->4->null
+// 3. 链表单指针法的应用
+import {
+  concatSortedLinkedlist,
+  pickRepeatLinkedlist,
+  deleteRepeatLinkedlist,
+} from "./single-pointer-method";
+
+const l1 = JSON.parse(JSON.stringify(list));
+const l2 = JSON.parse(JSON.stringify(list));
+const twoLists = concatSortedLinkedlist(l1, l2); // 1->1->1->1->2->2->3->4->4->4->4->4->4->4->null
 console.log("将两个有序链表合并为一个新的有序链表", twoLists);
-const nonRepeatLists = pickRepeatLinkedlist(twoLists);
-console.log("有序链表去重", nonRepeatLists);
-const list3 = new ListNode(1)
-const list3Node2 = list3.next = new ListNode(1)
-const list3Node3 = list3Node2.next = new ListNode(2)
-const list3Node4 = list3Node3.next = new ListNode(3)
-const list3Node5 = list3Node4.next = new ListNode(4)
-const list3Node6 = list3Node5.next = new ListNode(4)
-list3Node6.next = new ListNode(4)
-console.log('list3', list3)
-const emptyRepeatLists = deleteRepeatLinkedlist(list3)
-console.log('有序链表清空重复节点', emptyRepeatLists)
 
-// 4. 链表的多指针法的应用
-import { deleteNEnd } from "./mutil-pointer-method";
-// const delete2EndList = deleteNEnd(list3, 4)
-// console.log('删除链表的倒数第2个结点', delete2EndList)
+const l3 = JSON.parse(JSON.stringify(list));
+const nonRepeatLists = pickRepeatLinkedlist(l3);
+console.log("有序链表去重", nonRepeatLists); // 1->2->3->4->null
+
+const l4 = JSON.parse(JSON.stringify(list));
+const emptyRepeatLists = deleteRepeatLinkedlist(l4);
+console.log("有序链表清空重复节点", emptyRepeatLists); // 2->3->null
+
+// 4. 链表多指针法的应用
+import {
+  deleteNEnd,
+  reverseList,
+  reverseFromM2N,
+} from "./mutil-pointer-method";
+
+const l5 = JSON.parse(JSON.stringify(list));
+const delete2EndList = deleteNEnd(l5, 2);
+console.log("删除链表的倒数第2个结点", delete2EndList); // 1->1->2->3->4->4->null
+
+const l6 = JSON.parse(JSON.stringify(list));
+const reversedList = reverseList(l6);
+console.log("反转链表", reversedList); // 4->4->4->3->2->1->1->null
+
+const l7 = JSON.parse(JSON.stringify(list));
+const reverseFrom225 = reverseFromM2N(l7, 2, 5);
+console.log("翻转链表第2到5个元素", reverseFrom225); // 1->4->3->2->1->4->4->null
